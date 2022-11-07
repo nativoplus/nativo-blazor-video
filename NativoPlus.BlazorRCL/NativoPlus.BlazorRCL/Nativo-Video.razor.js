92a06385c1c77b71f4225248705b421655603edb
crisamaro24 <cristianamaro7@gmail.com>
2022-11-07 10:29:14 -0400

    Enhancements

--- NativoPlus.BlazorRCL/NativoPlus.BlazorRCL/Nativo-Video.razor.js
+++ NativoPlus.BlazorRCL/NativoPlus.BlazorRCL/Nativo-Video.razor.js
@@ -1,1 +1,11 @@
 ﻿// Add all JavaScript code for the Nativo-Video component here
+window.helperFunctions = {
+    muteVideo: function (videoElementId) {
+        var videoElement = document.getElementById(videoElementId);
+
+        if (videoElement) {
+            videoElement.muted = "muted";
+            console.log(videoElement + " was muted");
+        }
+    }
+}
